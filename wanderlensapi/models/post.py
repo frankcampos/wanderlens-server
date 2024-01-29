8921363b434e406a89798e0b5e93fed17e5da3de
Mason Austin <mason47austin@gmail.com>
2024-01-29 11:31:26 -0600

    created post model

---
+++ wanderlensapi/models/post.py
@@ -0,0 +1,9 @@
+from django.db import models
+from .user import User
+
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    image_url = models.URLField()
+    content = models.TextField()
