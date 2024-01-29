4acc3f33644e65cc25175462dc0dbac093ca5253
Mason Austin <mason47austin@gmail.com>
2024-01-29 11:36:23 -0600

    created comment model

---
+++ wanderlensapi/models/comment.py
@@ -0,0 +1,9 @@
+from django.db import models
+from .user import User
+from .post import Post
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
