dcb7c5ec5a844961a447d8ccf1f114523dfc6e89
Mason Austin <mason47austin@gmail.com>
2024-01-29 18:57:29 -0600

    created check user function

--- wanderlensapi/views/auth.py
+++ wanderlensapi/views/auth.py
@@ -24,3 +24,30 @@ def register_user(request):
         'bio': user.bio
     }
     return Response(data)
+
+@api_view(['POST'])
+def check_user(request):
+    '''Checks to see if User has Associated User
+
+    Method arguments:
+      request -- The full HTTP request object
+    '''
+    uid = request.data['uid']
+
+    # Use the built-in authenticate method to verify
+    # authenticate returns the user object or None if no user is found
+    user = User.objects.filter(uid=uid).first()
+
+    # If authentication was successful, respond with their token
+    if user is not None:
+        data = {
+            'id': user.id,
+            'name': user.name,
+            'uid': user.uid,
+            'bio': user.bio
+        }
+        return Response(data)
+    else:
+        # Bad login details were provided. So we can't log the user in.
+        data = { 'valid': False }
+        return Response(data)
