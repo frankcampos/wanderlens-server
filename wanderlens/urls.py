a2e540695a61cf1d4c37ed77293ba880c88daa45
caby-erin <caby27@gmail.com>
2024-01-29 19:50:01 -0600

    retrieve and list for posts

--- wanderlens/urls.py
+++ wanderlens/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from wanderlensapi.views import register_user, check_user, TagView
+from wanderlensapi.views import register_user, check_user, TagView, PostView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'tags', TagView, 'tag')
+router.register(r'posts', PostView, 'post')
+
 
 urlpatterns = [
     path('', include(router.urls)),
