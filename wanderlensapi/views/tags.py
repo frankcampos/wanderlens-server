7dc13ca1c6c90b792e9637bc17ba3d4ae6373e6f
Mason Austin <mason47austin@gmail.com>
2024-01-29 19:38:25 -0600

    created list for all tags

--- wanderlensapi/views/tags.py
+++ wanderlensapi/views/tags.py
@@ -7,16 +7,27 @@ from ..models import Tag
 class TagView(ViewSet):
 
     def retrieve(self, request, pk):
-        """Handle GET requests for a single order type
+        """Handle GET requests for a single tag
           
         returns:
-        Response -- JSON Serialzied order"""
+        Response -- JSON Serialzied tag"""
         try:
             tag = Tag.objects.get(pk=pk)
             serializer = TagSerializer(tag)
             return Response(serializer.data)
         except Tag.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+          
+    def list(self, request):
+        """Handle GET requests for every Order
+
+        Returns:
+            Response -- JSON serialized Orders
+        """
+        tags = Tag.objects.all()
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
+
 
 class TagSerializer(serializers.ModelSerializer):
     """JSON serializer for Order"""
